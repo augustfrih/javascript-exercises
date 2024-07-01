bd48588860bac1dba24b1116ff8a8c541f12025a
August <augustfrih@gmail.com>
2024-07-01 15:23:37 +0200

    Completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    
+    let i = 0;
+    let strings = [""]
 
-};
+    while (i < num) {
+        strings[i + 1] = strings[i] + string;
+        i++;
+    }
 
+    return strings[i];
+}
 // Do not edit below this line
 module.exports = repeatString;
