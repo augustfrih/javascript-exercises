16a62b13cbe1ac6ff3df2e096ab59466344cd3ad
August <augustfrih@gmail.com>
2024-07-02 12:45:52 +0200

    Completed exercise 03 JS

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,22 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    let length = string.length;
+    let strArray = [];
+    let strings = [];
+
+    for (i = 0; i < length; i++) {
+        strArray[i] = string.slice(i, (i + 1));
+    }
+
+    for (i = length; i >= 0; i--) {
+        if (i == length) {
+            strings[i] = "";
+        } else {
+            strings[i] = strings[i + 1] + strArray[i];
+        }
+    }
+
+    return strings[0];
 
 };
 
