33aa12e6b11b08fc050bfb82868ee2c63f792504
August <augustfrih@gmail.com>
2024-07-03 11:00:11 +0200

    completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let sum = 0;
 
+    if (x < 0 || y < 0 || typeof(x) != typeof(1) || typeof(y) != typeof(1)) {
+        return "ERROR"
+    }
+
+    for (i = x; i <= y; i++) {
+        sum = sum + i;
+    }
+
+    for (i = y; i <= x; i++) {
+        sum = sum + i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
