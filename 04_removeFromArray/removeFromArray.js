0c0bf2131dbcd56739548ac7f7e77913cf03c5cc
August <augustfrih@gmail.com>
2024-07-03 10:46:21 +0200

    Completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
 const removeFromArray = function() {
+
+    let length = arguments[0].length;
+    let argNum = arguments.length;
+
+    for (y = 1; y < argNum; y++) {
+        for (i = 0; i < length; i++) {
+            if (arguments[0][i] == arguments[y] && typeof(arguments[0][i]) == typeof(arguments[y])) {
+                arguments[0].splice(i, 1);
+                i = i - 1;
+            }
+        }
+    }
+    return arguments[0];
+
 };
 
 // Do not edit below this line
