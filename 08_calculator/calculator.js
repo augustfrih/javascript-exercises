ccac2bb8077d875e8fc1414a43d7c95ac8da445f
August <augustfrih@gmail.com>
2024-07-17 11:01:44 +0200

    Completed all functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -14,12 +14,21 @@ const multiply = function(arr) {
   return arr.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let x = 1;
+  for (i = 0; i < b; i++) {
+    x = x * a;
+  };
+  return x;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let arr = [];
+  for (i = 1; i <= a; i++) {
+    arr.push(i);
+  };
+  let x = arr.reduce((total, next) => total * next, 1);
+  return x;
 };
 
 // Do not edit below this line
