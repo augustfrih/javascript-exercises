964770923050c2eeb406b934f0564f7f1b92eaff
August <augustfrih@gmail.com>
2024-07-17 10:42:40 +0200

    Completed multiply and sum

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,12 +6,12 @@ const subtract = function(a, b) {
   return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current) => total * current, 1);
 };
 
 const power = function() {
