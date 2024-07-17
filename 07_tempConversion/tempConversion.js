6a716e2ee645654dceaefd2ea2d2c221b3075f8a
August <augustfrih@gmail.com>
2024-07-17 10:37:29 +0200

    Completed the add function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let temp = (x - 32) * (5 / 9);
+  let roundTemp = Number(temp.toFixed(1));
+
+  return roundTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  let temp = x * (9 / 5) + 32;
+  let roundTemp = Number(temp.toFixed(1));
+
+  return roundTemp;
 };
 
 // Do not edit below this line
